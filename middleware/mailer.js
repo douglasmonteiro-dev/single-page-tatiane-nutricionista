4269189404e9a05472466332d077fbdbed503f41
Douglas Monteiro <douglasmonteiro@icloud.com>
2020-12-28 18:18:43 -0300

    send email

---
+++ middleware/mailer.js
@@ -0,0 +1,44 @@
+const mailer = require('nodemailer');
+
+ 
+module.exports = (email, nome, mensagem, anexo) => {
+    const smtpTransport = mailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, //SSL/TLS
+        auth: {
+            user: 'tatianeribeiro.nutri@gmail.com',
+            pass: '6370445422!!'
+        }
+    })
+
+    
+    const mail = {
+        from: "Tatiane Nutricionista <tatianeribeiro.nutri@gmail.com>",
+        to: email,
+        subject: `${nome} baixe seu Ebook`,
+        text: mensagem,
+        //html: "<b>Opcionalmente, pode enviar como HTML</b>"
+    }
+    
+    if(anexo){
+        mail.attachments = [];
+        mail.attachments.push({
+            filename: 'ebook.pdf',
+            href: 'https://tatiane.ntr.br/ebook.pdf',
+            contentType: 'application/pdf'
+        })
+    }
+    
+    return new Promise((resolve, reject) => {
+        smtpTransport.sendMail(mail)
+            .then(response => {
+                smtpTransport.close();
+                return resolve(response);
+            })
+            .catch(error => {
+                smtpTransport.close();
+                return reject(error);
+            });
+    })
+}
--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,34 +1,43 @@
 import nextConnect from 'next-connect';
 import middleware from '../../middleware/database';
+import mailer from '../../middleware/mailer';
 const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
     const { data } = req.query;
-    const dataModel = {name: '', email: '', sent: false}
+    const dataModel = { name: '', email: '', sent: false }
     let doc = {}
-    if(data){
-        doc = await req.db.collection('ebook').findOne({email: date.email})
-    } else {
+    // if (data) {
+    //     doc = await req.db.collection('ebook').findOne({ email: date.email })
+    // } else {
+    //     doc = dataModel
+    // }
+    if (doc == null) {
         doc = dataModel
     }
-    if(doc == null){
-        doc = dataModel
-    }
+
     res.json(doc)
 });
 handler.post(async (req, res) => {
-    let doc = {}
+    // let doc = {}
     let data = req.body;
-    const dataModel = {name: '', email: '', sent: false}
+    // const dataModel = { name: '', email: '', sent: false }
 
-    if(data.email){
-        doc = await req.db.collection('ebook').findOne({email: data.email})
+    if (data.email) {
+        // doc = await req.db.collection('ebook').findOne({ email: data.email })
+        const nome = req.body.nome;
+        const email = req.body.email;
+        const mensagem = 'Espero que faça boa leitura';
+        const anexo = true;
+        mailer(email, nome, mensagem, anexo)
+            .then(response => res.json(response))
+            .catch(error => res.json(error));
     }
-    if(doc == null){
-        doc = await req.db.collection('ebook').updateOne({email: data.email}, {$set:data}, {upsert: true})
-        res.json({message: 'Enviado'});
-    }
+    // if (doc == null) {
+    //     doc = await req.db.collection('ebook').updateOne({ email: data.email }, { $set: data }, { upsert: true })
+    //     res.json({ message: 'Enviado' });
+    // }
 
-  })
+})
 export default handler;
